844a3b1614b92c6fb2638c6aea0df32a124f1cc6
cll008 <cll008@eng.ucsd.edu>
2018-05-03 17:46:44 -0700

    case fix

---
+++ eegplugin_getchanlocs.m
@@ -0,0 +1,40 @@
+% eegplugin_getChanLocs()
+%
+% Inputs:
+%   fig        - [integer] eeglab figure.
+%   trystrs    - [struct] "try" strings for menu callbacks.
+%   catchstrs  - [struct] "catch" strings for menu callbacks.
+%
+% Notes:
+%   To create a new plugin, simply create a file beginning with "eegplugin_"
+%   and place it in your eeglab folder. It will then be automatically
+%   detected by eeglab. See also this source code internal comments.
+%   For eeglab to return errors and add the function's results to
+%   the eeglab history, menu callback must be nested into "try" and
+%   a "catch" strings. 
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1.07  USA
+
+function vers = eegplugin_getChanLocs(fig, trystrs, catchstrs)
+
+vers = 'getChanLocs 1.60';
+if nargin < 3
+    error('eegplugin_getChanLocs requires 3 arguments');
+end;
+
+% create menu
+toolsMenu = findobj(fig, 'tag', 'tools');
+uimenu(toolsMenu, 'label', 'Locate electrodes in a 3D head image',...
+    'callback', 'gui_getChanLocs','separator','on');
---
+++ multi_template.m
@@ -0,0 +1,213 @@
+function varargout = multi_template(varargin)
+% MULTI_TEMPLATE MATLAB code for multi_template.fig
+%      MULTI_TEMPLATE by itself, creates a new MULTI_TEMPLATE or raises the
+%      existing singleton*.
+%
+%      H = MULTI_TEMPLATE returns the handle to a new MULTI_TEMPLATE or the handle to
+%      the existing singleton*.
+%
+%      MULTI_TEMPLATE('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MULTI_TEMPLATE.M with the given input arguments.
+%
+%      MULTI_TEMPLATE('Property','Value',...) creates a new MULTI_TEMPLATE or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before multi_template_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to multi_template_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help multi_template
+
+% Last Modified by GUIDE v2.5 03-May-2018 17:42:46
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @multi_template_OpeningFcn, ...
+                   'gui_OutputFcn',  @multi_template_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before multi_template is made visible.
+function multi_template_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to multi_template (see VARARGIN)
+
+% Choose default command line output for multi_template
+handles.output = 'Yes';
+
+% Update handles structure
+guidata(hObject, handles);
+
+% Insert custom Title and Text if specified by the user
+% Hint: when choosing keywords, be sure they are not easily confused 
+% with existing figure properties.  See the output of set(figure) for
+% a list of figure properties.
+if(nargin > 3)
+    for index = 1:2:(nargin-3),
+        if nargin-3==index, break, end
+        switch lower(varargin{index})
+         case 'title'
+          set(hObject, 'Name', varargin{index+1});
+         case 'string'
+          set(handles.text1, 'String', varargin{index+1});
+        end
+    end
+end
+
+% Determine the position of the dialog - centered on the callback figure
+% if available, else, centered on the screen
+FigPos=get(0,'DefaultFigurePosition');
+OldUnits = get(hObject, 'Units');
+set(hObject, 'Units', 'pixels');
+OldPos = get(hObject,'Position');
+FigWidth = OldPos(3);
+FigHeight = OldPos(4);
+if isempty(gcbf)
+    ScreenUnits=get(0,'Units');
+    set(0,'Units','pixels');
+    ScreenSize=get(0,'ScreenSize');
+    set(0,'Units',ScreenUnits);
+
+    FigPos(1)=1/2*(ScreenSize(3)-FigWidth);
+    FigPos(2)=2/3*(ScreenSize(4)-FigHeight);
+else
+    GCBFOldUnits = get(gcbf,'Units');
+    set(gcbf,'Units','pixels');
+    GCBFPos = get(gcbf,'Position');
+    set(gcbf,'Units',GCBFOldUnits);
+    FigPos(1:2) = [(GCBFPos(1) + GCBFPos(3) / 2) - FigWidth / 2, ...
+                   (GCBFPos(2) + GCBFPos(4) / 2) - FigHeight / 2];
+end
+FigPos(3:4)=[FigWidth FigHeight];
+set(hObject, 'Position', FigPos);
+set(hObject, 'Units', OldUnits);
+
+% Make the GUI modal
+set(handles.figure1,'WindowStyle','modal')
+set(handles.listbox1, 'String', evalin('base','refMats'));
+
+% UIWAIT makes multi_template wait for user response (see UIRESUME)
+uiwait(handles.figure1);
+
+% --- Outputs from this function are returned to the command line.
+function varargout = multi_template_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% The figure can be deleted now
+delete(handles.figure1);
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+contents = cellstr(get(handles.listbox1,'String'));
+handles.output = contents{get(handles.listbox1,'Value')};
+
+% Update handles structure
+guidata(hObject, handles);
+
+% Use UIRESUME instead of delete because the OutputFcn needs
+% to get the updated handles structure.
+uiresume(handles.figure1);
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+handles.output = get(hObject,'String');
+
+% Update handles structure
+guidata(hObject, handles);
+
+% Use UIRESUME instead of delete because the OutputFcn needs
+% to get the updated handles structure.
+uiresume(handles.figure1);
+
+
+% --- Executes when user attempts to close figure1.
+function figure1_CloseRequestFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+if isequal(get(hObject, 'waitstatus'), 'waiting')
+    % The GUI is still in UIWAIT, us UIRESUME
+    uiresume(hObject);
+else
+    % The GUI is no longer waiting, just close it
+    delete(hObject);
+end
+
+
+% --- Executes on key press over figure1 with no controls selected.
+function figure1_KeyPressFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Check for "enter" or "escape"
+if isequal(get(hObject,'CurrentKey'),'escape')
+    % User said no by hitting escape
+    handles.output = 'No';
+    
+    % Update handles structure
+    guidata(hObject, handles);
+    
+    uiresume(handles.figure1);
+end    
+    
+if isequal(get(hObject,'CurrentKey'),'return')
+    uiresume(handles.figure1);
+end
+
+
+% --- Executes on selection change in listbox1.
+function listbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns listbox1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from listbox1
+
+
+% --- Executes during object creation, after setting all properties.
+function listbox1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
